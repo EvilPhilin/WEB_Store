cdc4f4c72a1fd1434faa5f16163cfcf0cb81309e
Vladislav Nevskiy <VNevskii650@gmail.com>
2021-04-23 18:51:12 +0200

    One more fix

--- src/common/commonInterfaces.ts
+++ src/common/commonInterfaces.ts
@@ -1,21 +1,21 @@
-import {Customer,Order,Item} from "./commonClasses"
+import { Customer, Order, Item } from "./commonClasses";
 
-
-interface IDataBaseController{
-    notifyPushCustomer(cust:Customer):void;
-    notifyPushOrder(order:Order):void;
-    notifyGetOrderInfo(orderId:number):Order;
-    notifyGetItemInfdadawdao(itemId:number):void;
-    notifyGetListOfItems():Array<Item>;
-    notifyGetCustomerOrders(custId:number):Array<Order>
-    notifyGetIdOfCustomers():Set<number>
-    notifyGetIdOfOrders():Set<number>
-    
+interface IDataBaseController {
+  notifyPushCustomer(cust: Customer): void;
+  notifyPushOrder(order: Order): void;
+  notifyGetOrderInfo(orderId: number): void;
+  notifyGetItemInfo(itemId: number): void;
+  notifyGetListOfItems(): void;
+  notifyGetCustomerOrders(custId: number): void;
+  notifyGetIdOfCustomers(): void;
+  notifyGetIdOfOrders(): void;
 }
 
-interface IViewController{
-    updateOrderInfo(order:Order):void;
-    updateItemInfo(item:Item):void;
-    updateListOfItems(items:Array<Item>):void;
-    updateCustomerOrders(orders:Array<Order>):void;
+interface IViewController {
+  updateOrderInfo(order: Order): void;
+  updateItemInfo(item: Item): void;
+  updateListOfItems(items: Array<Item>): void;
+  updateCustomerOrders(orders: Array<Order>): void;
+  updateIdOfCustomers(data: Set<number>): void;
+  updateIdOfOrders(data: Set<number>): void;
 }
