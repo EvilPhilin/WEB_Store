b43615b67ea38845b1a0d9370a08586a1b3b6cb7
EvilPhilin <80574243+EvilPhilin@users.noreply.github.com>
2021-05-01 10:54:58 +0200

    testing connection to cluster 1

--- back/database.js
+++ back/database.js
@@ -1,32 +1,33 @@
 "use strict";
-//const Mongoose = require('mongoose');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var Mongoose = require('mongoose');
 var Express = require('express');
-//import Category from './category';
-//import Customer from './customer';
-//import Type from './model';
-//import Order from './order';
-//let database = Mongoose.Connection;
-//const url: string = "mongodb://localhost:27017/store";
-// Mongoose.connect(url,
-// {
-//     useNewUrlParser: true,
-//     useFindAndModify: true,
-//     useUnifiedTopology: true,
-//     useCreateIndex: true,
-// });
-// database = Mongoose.connection;
-// database.once("open", function()
-// {
-//     console.log("Connected to database");
-// });
-// database.on("error", function()
-// {
-//     console.log("Error connecting to database");
-// });
-// Category.create({ id_category: 1, name: 'Keyboards' });
-// Customer.create({ id_customer: 1, fname: 'Alexei', lname: 'Stepanov', date_of_birth: '1/1/1', location: 'Uganda'});
-// Type.create({ id_model: 1, name: 'Ya tebe shas potikayu', price: 199, category: 1, storage: 50});
-// Order.create({ id_order: 1, customer: 1, type: 1, date_of_order: '1/1/1', date_of_delivery: '2/1/1', delivery_type: 'Airdrop'});
+var category_1 = __importDefault(require("./category"));
+var customer_1 = __importDefault(require("./customer"));
+var model_1 = __importDefault(require("./model"));
+var order_1 = __importDefault(require("./order"));
+var database = Mongoose.Connection;
+var url = "mongodb+srv://Evil_Philin:83913932133@webstoreproject.1rfah.mongodb.net/web_store?retryWrites=true&w=majority";
+Mongoose.connect(url, {
+    useNewUrlParser: true,
+    useFindAndModify: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+});
+database = Mongoose.connection;
+database.once("open", function () {
+    console.log("Connected to database");
+});
+database.on("error", function () {
+    console.log("Error connecting to database");
+});
+category_1.default.create({ id_category: 1, name: 'Keyboards' });
+customer_1.default.create({ id_customer: 1, fname: 'Alexei', lname: 'Stepanov', date_of_birth: '1/1/1', location: 'Uganda' });
+model_1.default.create({ id_model: 1, name: 'Ya tebe shas potikayu', price: 199, category: 1, storage: 50 });
+order_1.default.create({ id_order: 1, customer: 1, type: 1, date_of_order: '1/1/1', date_of_delivery: '2/1/1', delivery_type: 'Airdrop' });
 var app = Express();
 var http = require('http').Server(app);
 app.use(function (request, response) {
@@ -36,4 +37,4 @@ var port = process.env.PORT || 5000;
 http.listen(port, function () {
     console.log('listening on port ' + port);
 });
-//Mongoose.disconnect();
+Mongoose.disconnect();
--- back/database.ts
+++ back/database.ts
@@ -1,38 +1,39 @@
-//const Mongoose = require('mongoose');
+const Mongoose = require('mongoose');
 const Express = require('express');
 
-//import Category from './category';
-//import Customer from './customer';
-//import Type from './model';
-//import Order from './order';
+import Category from './category';
+import Customer from './customer';
+import Type from './model';
+import Order from './order';
 
-//let database = Mongoose.Connection;
+let database = Mongoose.Connection;
 
-//const url: string = "mongodb://localhost:27017/store";
+const url: string = "mongodb+srv://Evil_Philin:83913932133@webstoreproject.1rfah.mongodb.net/web_store?retryWrites=true&w=majority";
 
-// Mongoose.connect(url,
-// {
-//     useNewUrlParser: true,
-//     useFindAndModify: true,
-//     useUnifiedTopology: true,
-//     useCreateIndex: true,
-// });
-// database = Mongoose.connection;
+Mongoose.connect(url,
+{
+    useNewUrlParser: true,
+    useFindAndModify: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+});
 
-// database.once("open", function()
-// {
-//     console.log("Connected to database");
-// });
+database = Mongoose.connection;
 
-// database.on("error", function()
-// {
-//     console.log("Error connecting to database");
-// });
+database.once("open", function()
+{
+    console.log("Connected to database");
+});
 
-// Category.create({ id_category: 1, name: 'Keyboards' });
-// Customer.create({ id_customer: 1, fname: 'Alexei', lname: 'Stepanov', date_of_birth: '1/1/1', location: 'Uganda'});
-// Type.create({ id_model: 1, name: 'Ya tebe shas potikayu', price: 199, category: 1, storage: 50});
-// Order.create({ id_order: 1, customer: 1, type: 1, date_of_order: '1/1/1', date_of_delivery: '2/1/1', delivery_type: 'Airdrop'});
+database.on("error", function()
+{
+    console.log("Error connecting to database");
+});
+
+Category.create({ id_category: 1, name: 'Keyboards' });
+Customer.create({ id_customer: 1, fname: 'Alexei', lname: 'Stepanov', date_of_birth: '1/1/1', location: 'Uganda'});
+Type.create({ id_model: 1, name: 'Ya tebe shas potikayu', price: 199, category: 1, storage: 50});
+Order.create({ id_order: 1, customer: 1, type: 1, date_of_order: '1/1/1', date_of_delivery: '2/1/1', delivery_type: 'Airdrop'});
 
 const app = Express();
 const http = require('http').Server(app);
@@ -48,4 +49,4 @@ http.listen(port, function()
 {
     console.log('listening on port ' + port);
 });
-//Mongoose.disconnect();
+Mongoose.disconnect();
