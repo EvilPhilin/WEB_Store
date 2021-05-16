a886b8fd9a17bb613112edd791b4213a86a71cf3
EvilPhilin <kurdjaev_sergey@rambler.ru>
2021-05-16 17:38:05 +0200

    1

--- back/database.js
+++ back/database.js
@@ -19,9 +19,9 @@ database.on("error", function () {
 });
 const app = Express();
 const http = new Http.Server(app);
-app.use(Express.static(Path.resolve(__dirname, '../build')));
+app.use(Express.static(Path.join(__dirname, '/build')));
 app.get('/', (request, response) => {
-    response.sendFile(Path.resolve(__dirname, '../build', 'index.html'));
+    response.sendFile(Path.join(__dirname, '/build', 'index.html'));
     response.end();
 });
 app.get('/func', async (request, response) => {
